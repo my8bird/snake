061e62288e4f2dfe8a336c25a820ee5889c4b9f5
Nathan Landis <my8bird@gmail.com>
2012-06-15 19:30:54 -0500

    Swith to snake instead of pake since that sounds better

--- snake.py
+++ snake.py
@@ -4,55 +4,56 @@ import imp
 import argparse
 from collections import OrderedDict
 
-
-def getTasks(pakefile):
-   items = pakefile.__dict__.viewitems()
+def getTasks(snakefile):
+   items = snakefile.__dict__.viewitems()
    tasks = {name.split('_', 1)[1]: func for name, func in items \
             if name.startswith('task_')}
 
    return tasks
 
 
-def getPakefile():
-   assert os.path.exists('Pakefile'), 'Pakefile not found'
-   pakefile = imp.load_source('Pakefile', os.path.abspath('Pakefile'))
+def getSnakefile():
+   assert os.path.exists('Snakefile'), 'Snakefile not found'
+   snakefile = imp.load_source('Snakefile', os.path.abspath('Snakefile'), open('Snakefile'))
 
-   return (pakefile, getTasks(pakefile))
+   return (snakefile, getTasks(snakefile))
 
 
-def buildArgParser(pakefile, task):
-   parser = argparse.ArgumentParser(prog = 'pake ' + task)
+def buildArgParser(snakefile, task):
+   parser = argparse.ArgumentParser(prog = 'snake ' + task)
    # Find the tasks option setter if there is one
-   add_options = getattr(pakefile, 'options_'+task, None)
+   add_options = getattr(snakefile, 'options_'+task, None)
    if add_options is not None:
       # The task does take args so pass them in
       add_options(parser)
 
    return parser
 
-def printTasks(pakefile, tasks):
+def printTasks(snakefile, tasks):
    print "Tasks:"
    print "  " + "\n  ".join(tasks)
 
 
-def parseOptions(pakefile, tasks):
+def parseOptions(snakefile, tasks):
    # Break passed args in to sub lists per supplied task
    # This allows for:
    #    test -w build --all
    # Where -w is only for test and --all is only for build
    args_per_task = OrderedDict()
+   cur_task = None
    for arg in sys.argv[1:]:
       if arg in tasks:
          cur_task = arg
          args_per_task[arg] = []
       else:
+         assert cur_task is not None, 'Tasks must come before args'
          args_per_task[cur_task].append(arg)
 
    # Parse the args for each task providing nice failures and help text
    parsed_per_task = OrderedDict()
    for task, args in args_per_task.viewitems():
       # Get a parser that knows all of fields in question
-      parser = buildArgParser(pakefile, task)
+      parser = buildArgParser(snakefile, task)
 
       # Store the options with the task
       # Even if no options were passed in the key must be set so that we
@@ -63,14 +64,14 @@ def parseOptions(pakefile, tasks):
 
 
 if __name__ == '__main__':
-   pakefile, task_funcs = getPakefile()
+   snakefile, task_funcs = getSnakefile()
 
-   tasks = parseOptions(pakefile, task_funcs)
+   tasks = parseOptions(snakefile, task_funcs)
 
    if 0 == len(tasks):
-      printTasks(pakefile, task_funcs.keys())
+      printTasks(snakefile, task_funcs.keys())
       exit(0)
 
    for task, args in tasks.viewitems():
       # Call the task with the supplied args
-      getattr(pakefile, 'task_' + task)(args)
+      getattr(snakefile, 'task_' + task)(args)
